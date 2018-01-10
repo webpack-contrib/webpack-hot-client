87de9e600141c10adc05d5c4f3219bb091795884
JounQin <admin@1stg.me>
2018-01-10 21:42:54 +0800

    only enable webpack-hot-client for targeting web (#3)

--- HotClientEntryPlugin.js
+++ HotClientEntryPlugin.js
@@ -6,8 +6,13 @@ const uuid = require('uuid/v4');
 
 module.exports = class HotClientEntryPlugin {
   // eslint-disable-next-line class-methods-use-this
-  toPlugin(context, item, name) {
-    const hotEntry = [`webpack-hot-client/client?${name || uuid()}`];
+  toPlugin(compiler, context, item, name) {
+    const hotEntry = [];
+
+    if (compiler.options.target === 'web') {
+      hotEntry.push(`webpack-hot-client/client?${name || uuid()}`);
+    }
+
     const entry = hotEntry.concat(item);
 
     return new MultiEntryPlugin(context, entry, name);
@@ -16,9 +21,9 @@ module.exports = class HotClientEntryPlugin {
   apply(compiler) {
     compiler.plugin('entry-option', (context, entry) => {
       if (typeof entry === 'string' || Array.isArray(entry)) {
-        compiler.apply(this.toPlugin(context, entry, 'main'));
+        compiler.apply(this.toPlugin(compiler, context, entry, 'main'));
       } else if (typeof entry === 'object') {
-        Object.keys(entry).forEach(name => compiler.apply(this.toPlugin(context, entry[name], name)));
+        Object.keys(entry).forEach(name => compiler.apply(this.toPlugin(compiler, context, entry[name], name)));
       } else if (typeof entry === 'function') {
         // TODO support functions
         compiler.apply(new DynamicEntryPlugin(context, entry));
